db687110c8e56aa3efda5bcd94b146702a6854ee
Bruno Borges <brunoborges2001@gmail.com>
2023-07-31 18:37:07 -0300

    remove cdn

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -1,12 +1,11 @@
 import SanityClient from 'next-sanity-client'
 
-
 const client = new SanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   apiVersion: '2021-10-21',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
-  useCdn: process.env.NODE_ENV === 'production',
+  useCdn: false,
 })
 
 export default client
